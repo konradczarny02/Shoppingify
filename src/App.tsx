e03abbf199447cc0a87c330da8113216f39f9a08
Konrad Czarny <konradcz02@gmail.com>
2023-01-03 21:44:14 +0100

    feat: add fonts and default css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 export const App = () => {
   return (
     <div>
-      <span className="material-icons">&#xE87C;</span>
       <h1 className="text-5xl text-red-300">Hello World</h1>
     </div>
   );

3ac055cd0d03215ee6dc884b9a5e73954455cfb1
Konrad Czarny <konradcz02@gmail.com>
2023-01-03 20:58:13 +0100

    feat: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
+
 export const App = () => {
-    return (
-        <h1 className="text-5xl text-red-300">Hello World</h1>
-        )
-}
+  return <h1 className="text-5xl text-red-300">Hello World</h1>;
+};

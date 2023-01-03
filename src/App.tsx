b28a5233e351dd8159ac63da72d52637155d0a4e
Konrad Czarny <konradcz02@gmail.com>
2023-01-03 21:27:05 +0100

    feat: add material icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export const App = () => {
-  return <h1 className="text-5xl text-red-300">Hello World</h1>;
+  return (
+    <div>
+      <span className="material-icons">&#xE87C;</span>
+      <h1 className="text-5xl text-red-300">Hello World</h1>
+    </div>
+  );
 };
